0f9492b7e587e60afb9dd0c9179d5c5665f01a95
Nikhil Gupta <me@nikhgupta.com>
2021-07-02 11:14:48 +0530

    initial commit

---
+++ lib/abunai.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "abunai/version"
+
+module Abunai
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/abunai/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Abunai
+  VERSION = "0.1.0"
+end
